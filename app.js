b1e989abf7a344b8f2a5810ae9e8fdd79d7a60cb
Luca <95436769+Lukenssss@users.noreply.github.com>
2023-03-01 04:37:40 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const socket_io_1 = require("socket.io");
+const http_1 = require("http");
+const httpServer = (0, http_1.createServer)();
+const io = new socket_io_1.Server(httpServer, {});
+io.on('connection', (socket) => {
+    socket.on('message', (e) => {
+        console.log(e);
+        io.emit('message', e);
+    });
+});
+httpServer.listen(5000, '192.168.86.207', () => {
+    console.log('Done listening');
+});
